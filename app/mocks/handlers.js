787d2cd56354452a1f60a188a5b11eb88b95850c
Daniel Martins <martinsda@gmail.com>
2025-05-14 17:24:12 -0300

    add post, put, delete endpoints

--- app/mocks/handlers.js
+++ app/mocks/handlers.js
@@ -1,26 +1,57 @@
 import { rest } from "msw";
 
-const products = [
-  {
-    id: 1,
-    nome: "Produto 1",
-    categoria: "Eletrônicos",
-    preco: 299.99,
-    descricao: "Descrição do Produto 1",
-    imagem: "https://via.placeholder.com/300",
-  },
-  {
-    id: 2,
-    nome: "Produto 2",
-    categoria: "Livros",
-    preco: 49.9,
-    descricao: "Descrição do Produto 2",
-    imagem: "https://via.placeholder.com/300",
-  },
-];
+import { products } from "@/app/mocks/products";
 
 export const handlers = [
   rest.get("http://localhost:3000/api/produtos", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(products));
+  }),
+
+  rest.post("http://localhost:3000/api/produtos", async (req, res, ctx) => {
+    const newProduct = await req.json();
+
+    const createdProduct = {
+      ...newProduct,
+      id: products.length + 1,
+    };
+
+    products.push(createdProduct);
+
+    return res(ctx.status(201), ctx.json(createdProduct));
+  }),
+
+  rest.put("http://localhost:3000/api/produtos/:id", async (req, res, ctx) => {
+    const { id } = req.params;
+    const updatedProduct = await req.json();
+    const index = products.findIndex((p) => p.id === parseInt(id));
+
+    if (index === -1) {
+      return res(
+        ctx.status(404),
+        ctx.json({ message: "Produto não encontrado" })
+      );
+    }
+
+    products[index] = {
+      ...products[index],
+      ...updatedProduct,
+      id: products[index].id,
+    };
+    return res(ctx.status(200), ctx.json(products[index]));
+  }),
+
+  rest.delete("http://localhost:3000/api/produtos/:id", (req, res, ctx) => {
+    const { id } = req.params;
+    const index = products.findIndex((p) => p.id === parseInt(id));
+
+    if (index === -1) {
+      return res(
+        ctx.status(404),
+        ctx.json({ message: "Produto não encontrado" })
+      );
+    }
+
+    products.splice(index, 1);
+    return res(ctx.status(204));
   }),
 ];
