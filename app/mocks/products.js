33d282fa6b48c9d97560e0a5c2225cd61fdb15b6
Daniel Martins <martinsda@gmail.com>
2025-05-15 07:01:02 -0300

    add more mocked products

--- app/mocks/products.js
+++ app/mocks/products.js
@@ -44,4 +44,40 @@ export const products = [
       "Os AirPods 4 foram redesenhados para proporcionar conforto o dia todo e maior estabilidade. Com um acabamento refinado, haste menor e controles por toque para música ou chamadas.",
     imagemURL: "https://m.media-amazon.com/images/I/415J3RCvdKL._AC_SX679_.jpg",
   },
+  {
+    id: 6,
+    nome: "iMac",
+    categoria: "iMac",
+    preco: 11999,
+    descricao:
+      "O iMac é um computador tudo em um com design incrivelmente fino, que vem em sete cores vibrantes para combinar com qualquer ambiente",
+    imagemURL: "https://m.media-amazon.com/images/I/51DA8b7jsEL._AC_SX679_.jpg",
+  },
+  {
+    id: 7,
+    nome: "iPhone 16 Pro",
+    categoria: "iPhone",
+    preco: 10499,
+    descricao:
+      "O iPhone 16 Pro tem uma estrutura leve e robusta em titânio, uma tela Super Retina XDR maior, de 6,3 polegadas, e a parte frontal é em Ceramic Shield de última geração, duas vezes mais resistente que qualquer vidro de smartphone.",
+    imagemURL: "https://m.media-amazon.com/images/I/61irT-Uu+8L._AC_SX679_.jpg",
+  },
+  {
+    id: 8,
+    nome: "iPad mini",
+    categoria: "iPad",
+    preco: 6299,
+    descricao:
+      "O fantástico ecrã Liquid Retina inclui tecnologias avançadas, como vasta gama de cores P3, True Tone e uma refletividade extremamente baixa, que tornam tudo mais deslumbrante.",
+    imagemURL: "https://m.media-amazon.com/images/I/71H7417TSdL._AC_SX679_.jpg",
+  },
+  {
+    id: 9,
+    nome: "Apple Watch Ultra 2",
+    categoria: "Apple Watch",
+    preco: 10499,
+    descricao:
+      "O relógio definitivo para esportes e aventura tem caixa robusta de titânio de 49 mm com 100m de resistência à água e classificação IP6X de resistência à poeira. Agora em preto",
+    imagemURL: "https://m.media-amazon.com/images/I/61M0IYhqH8L._AC_SX679_.jpg",
+  },
 ];
