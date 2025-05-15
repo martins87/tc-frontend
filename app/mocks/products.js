05cb3285e132a7a259a25dfb5a6f7d599bfdbca8
Daniel Martins <martinsda@gmail.com>
2025-05-15 07:26:17 -0300

    add more mocked products

--- app/mocks/products.js
+++ app/mocks/products.js
@@ -38,7 +38,7 @@ export const products = [
   {
     id: 5,
     nome: "AirPods 4",
-    categoria: "AirPod",
+    categoria: "AirPods",
     preco: 1099,
     descricao:
       "Os AirPods 4 foram redesenhados para proporcionar conforto o dia todo e maior estabilidade. Com um acabamento refinado, haste menor e controles por toque para música ou chamadas.",
@@ -79,5 +79,60 @@ export const products = [
     descricao:
       "O relógio definitivo para esportes e aventura tem caixa robusta de titânio de 49 mm com 100m de resistência à água e classificação IP6X de resistência à poeira. Agora em preto",
     imagemURL: "https://m.media-amazon.com/images/I/61M0IYhqH8L._AC_SX679_.jpg",
+  },
+  {
+    id: 10,
+    nome: "AirPods Pro 2",
+    categoria: "AirPods",
+    preco: 2599,
+    descricao:
+      "Cancelamento Ativo de Ruído até 2x melhor. Áudio Adaptativo que ajusta o controle de ruído em diferentes situações. Áudio Espacial Personalizado para envolver você. E uma nova experiência completa para testar e auxiliar sua audição, além de minimizar sua exposição a ruídos altos.",
+    imagemURL: "https://m.media-amazon.com/images/I/41YmidweMtL._AC_SX679_.jpg",
+  },
+  {
+    id: 11,
+    nome: "Mac mini",
+    categoria: "Mac",
+    preco: 7499,
+    descricao:
+      "Com pouco mais de 12 cm de largura e profundidade e menos de 1 kg, o Mac mini fica perfeito embaixo do monitor e pode ser levado e instalado em qualquer lugar.",
+    imagemURL: "https://m.media-amazon.com/images/I/41rPlEaOoKL._AC_SX679_.jpg",
+  },
+  {
+    id: 12,
+    nome: "iPhone 15",
+    categoria: "iPhone",
+    preco: 6499,
+    descricao:
+      "6,1 polegadas com vidro resistente colorido por infusão e design em alumínio com a parte da frente em Ceramic Shield.",
+    imagemURL: "https://m.media-amazon.com/images/I/416MG51rNgL._AC_SX679_.jpg",
+  },
+  {
+    id: 13,
+    nome: "iPad Air 11 pol.",
+    categoria: "iPad",
+    preco: 7499,
+    descricao:
+      "O espetacular ecrã Liquid Retina inclui tecnologias avançadas, como vasta gama de cores P3, True Tone e uma refletividade extremamente baixa, que tornam tudo mais deslumbrante.",
+    imagemURL:
+      "https://m.media-amazon.com/images/I/51qEyErGPaL.__AC_SY445_SX342_QL70_ML2_.jpg",
+  },
+  {
+    id: 14,
+    nome: "Apple Watch SE",
+    categoria: "Apple Watch",
+    preco: 3299,
+    descricao:
+      "Todos os recursos essenciais para você se movimentar, manter a conexão, monitorar sua saúde e ficar em segurança. Com Detecção de Queda e métricas de treino aprimoradas, o Apple Watch SE é cheio de possibilidades ao alcance do seu pulso.",
+    imagemURL: "https://m.media-amazon.com/images/I/41-c3pK4VvL._AC_SX679_.jpg",
+  },
+  {
+    id: 15,
+    nome: "AirPods Max - Meia-noite",
+    categoria: "AirPods",
+    preco: 6599,
+    descricao:
+      "O driver dinâmico criado pela Apple produz som de alta-fidelidade. O áudio computacional combina design acústico, chip H1 e software da Apple para experiências de áudio inovadoras.",
+    imagemURL: "https://m.media-amazon.com/images/I/51v0UFOTCrL._AC_SX679_.jpg",
   },
 ];
